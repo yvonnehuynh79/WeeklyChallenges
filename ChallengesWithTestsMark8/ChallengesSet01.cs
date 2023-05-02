3b9d6e0b1e2b62e7fa664ed3710632c75ad65304
yvonnehuynh79 <yvonnehuynh888@gmail.com>
2023-05-02 14:29:18 -0700

    set 1 & 2

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -5,45 +5,40 @@ namespace ChallengesWithTestsMark8
     public class ChallengesSet01
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
+           
         {
-            if(num1 == num2)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return num1 == num2;
+            
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            return minuend - subtrahend;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return Math.Min(number2, number2);
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            return (String.IsNullOrEmpty(nameOfPerson)) ? $"Hello!" : $"Hello, {nameOfPerson}!";
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,75 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return Char.IsLetter(c);
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            return vals.Length % 2 == 0;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+           return number % 2 == 0;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return num % 2 != 0;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            return numbers?.Any() ?? false ? numbers.Min() + numbers.Max() : new double { };
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            return (str1.Length <= str2.Length) ? str1.Length : str2.Length;
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers?.Sum() ?? 0;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+
+            while (numbers != null)
+            {
+                int sum = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] % 2 == 0)
+                    {
+                        sum += numbers[i];
+                    }
+                    
+                }
+                return sum;
+            }
+            return 0;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+            return (numbers.Sum() % 2 != 0);
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            int count = 0;
+            for ( int i = 1; i < number; i += 2)
+            {
+                count++;
+            }
+            return count;
         }
     }
 }
